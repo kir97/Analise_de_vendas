68a5451244cb56ab4462bd8d1fbfc6acf8ab46a2
Hokko-ops <lucio.junior99.lf@gmail.com>
2025-01-23 21:00:27 -0300

    Melhorias no código de análise de vendas: Adicionada a função de fechamento da conexão com o banco de dados após o uso. Implementado tratamento de erros na conexão com o banco de dados, garantindo robustez no carregamento dos dados. Otimização da função de cálculo de faturamento para evitar redundância no cálculo. Pequenas melhorias de legibilidade e organização geral do código.

--- scripts/analise_vendas.py
+++ scripts/analise_vendas.py
@@ -3,38 +3,37 @@ import sqlite3
 
 def carregar_dados_do_banco():
     # Conectar ao banco de dados SQLite
-    conn = sqlite3.connect("./data/dataset_vendas_ficticias.db")  # Ajuste o caminho conforme necessário
-    df = pd.read_sql_query("SELECT * FROM registro_vendas;", conn)
+    try:
+        conn = sqlite3.connect("./data/dataset_vendas_ficticias.db")  # Ajuste o caminho conforme necessário
+        df = pd.read_sql_query("SELECT * FROM registro_vendas;", conn)
+    except sqlite3.Error as e:
+        print(f"Erro ao conectar ao banco de dados: {e}")
+        return None
+    finally:
+        conn.close()  # Fechar a conexão após o uso
     return df
 
 def calcular_faturamento(df):
     # Calcular o faturamento total, considerando o desconto aplicado
     df["Faturamento"] = (df["Quantidade Vendida"] * df["Preço Unitário"]) - df["Desconto Aplicado"]
-    faturamento_total = df["Faturamento"].sum()
-    return faturamento_total
+    return df["Faturamento"].sum()
 
 def produto_mais_vendido(df):
     # Identificar o produto mais vendido
     produto_quantidade = df.groupby("Produto")["Quantidade Vendida"].sum()
-    produto_mais_vendido = produto_quantidade.idxmax()
-    quantidade_mais_vendida = produto_quantidade.max()
-    return produto_mais_vendido, quantidade_mais_vendida
+    return produto_quantidade.idxmax(), produto_quantidade.max()
 
 def calcular_vendas_mensais(df):
     # Calcular as vendas mensais
-    df["Data da Venda"] = pd.to_datetime(df["Data da Venda"])  # Remover o 'unit=s'
+    df["Data da Venda"] = pd.to_datetime(df["Data da Venda"])  
     df["Mês"] = df["Data da Venda"].dt.to_period("M")  # Convertendo para período mensal
-    df["Faturamento"] = (df["Quantidade Vendida"] * df["Preço Unitário"]) - df["Desconto Aplicado"]
-    
-    # Agrupando e somando o faturamento por mês
-    vendas_mensais = df.groupby("Mês")["Faturamento"].sum()
+    return df.groupby("Mês")["Faturamento"].sum()
 
-    return vendas_mensais
-
-# Função principal para execução do script
 def main():
     # Carregar dados
     df = carregar_dados_do_banco()
+    if df is None:
+        return  # Se não conseguir carregar os dados, interrompe a execução
 
     # Calcular o faturamento total
     faturamento_total = calcular_faturamento(df)
